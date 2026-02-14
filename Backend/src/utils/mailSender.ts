d4f456560f8f3490261c9fa2ed79384d27a38781
Rituraj-30 <rutu2930@gmail.com>
2026-02-14 09:18:21 +0530

    Update mailSender.ts

--- Backend/src/utils/mailSender.ts
+++ Backend/src/utils/mailSender.ts
@@ -1,74 +1,74 @@
 
 
-import { Resend } from "resend";
+// import { Resend } from "resend";
 
 
-const resend = new Resend(process.env.RESEND_API_KEY!);
+// const resend = new Resend(process.env.RESEND_API_KEY!);
 
-interface SendMailProps {
-  to: string;
-  subject: string;
-  html: string;
-}
+// interface SendMailProps {
+//   to: string;
+//   subject: string;
+//   html: string;
+// }
 
-const mailSender = async ({ to, subject, html }: SendMailProps) => {
-  try {
-    const response = await resend.emails.send({
-      from: process.env.MAIL_FROM!,
-      to,
-      subject,
-      html,
-    });
-
-    return response;
-  } catch (error) {
-    console.error("❌ Email send error:", error);
-    throw error;
-  }
-};
-
-export default mailSender;
-
-
-
-
-// import nodemailer, { Transporter, SentMessageInfo } from "nodemailer";
-
-// interface MailSenderResponse extends SentMessageInfo {}
-
-// const mailSender = async (
-//   email: string,
-//   title: string,
-//   body: string
-// ): Promise<MailSenderResponse | undefined> => {
+// const mailSender = async ({ to, subject, html }: SendMailProps) => {
 //   try {
-//        const transporter = nodemailer.createTransport({
-//       host: "smtp.gmail.com",
-//       port: 465,            
-//       secure: true,        
-//       auth: {
-//         user: process.env.MAIL_USER, 
-//         pass: process.env.MAIL_PASS, 
-//       },
-//       connectionTimeout: 30000,
-//       greetingTimeout: 30000,
-//       socketTimeout: 30000,
+//     const response = await resend.emails.send({
+//       from: process.env.MAIL_FROM!,
+//       to,
+//       subject,
+//       html,
 //     });
 
-
-//     const info = await transporter.sendMail({
-//       from: `"CodeStudy" <${process.env.MAIL_USER}>`,
-//       to: email,
-//       subject: title,
-//       html: body,
-//     });
-
-//     return info;
+//     return response;
 //   } catch (error) {
-//     console.error("Error while sending mail:", email);
-//     console.error(error);
-//     return undefined;
+//     console.error("❌ Email send error:", error);
+//     throw error;
 //   }
 // };
 
 // export default mailSender;
+
+
+
+
+import nodemailer, { Transporter, SentMessageInfo } from "nodemailer";
+
+interface MailSenderResponse extends SentMessageInfo {}
+
+const mailSender = async (
+  email: string,
+  title: string,
+  body: string
+): Promise<MailSenderResponse | undefined> => {
+  try {
+       const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,            
+      secure: true,        
+      auth: {
+        user: process.env.MAIL_USER, 
+        pass: process.env.MAIL_PASS, 
+      },
+      connectionTimeout: 30000,
+      greetingTimeout: 30000,
+      socketTimeout: 30000,
+    });
+
+
+    const info = await transporter.sendMail({
+      from: `"CodeStudy" <${process.env.MAIL_USER}>`,
+      to: email,
+      subject: title,
+      html: body,
+    });
+
+    return info;
+  } catch (error) {
+    console.error("Error while sending mail:", email);
+    console.error(error);
+    return undefined;
+  }
+};
+
+export default mailSender;
