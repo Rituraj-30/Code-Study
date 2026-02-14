b6bab416b6652d603929ef529937f1d7fc14253e
Rituraj-30 <rutu2930@gmail.com>
2026-02-14 11:41:52 +0530

    Update mailSender.ts

--- Backend/src/utils/mailSender.ts
+++ Backend/src/utils/mailSender.ts
@@ -11,19 +11,13 @@ const mailSender = async (
   body: string
 ): Promise<MailSenderResponse | undefined> => {
   try {
-     const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false, 
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false, 
-    minVersion: "TLSv1.2"
-  }
-});
+let transporter = nodemailer.createTransport({
+      service: "gmail", 
+      auth: {
+        user: process.env.MAIL_USER, 
+        pass: process.env.MAIL_PASS, 
+      },
+    });
 
     const info = await transporter.sendMail({
       from: `"CodeStudy" <${process.env.MAIL_USER}>`,
