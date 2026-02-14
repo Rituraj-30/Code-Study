8ecb1e722571f00e947d6cd58439cbdc62806f58
Rituraj-30 <rutu2930@gmail.com>
2026-02-14 07:44:05 +0530

    Update razorpay.ts

--- Backend/src/config/razorpay.ts
+++ Backend/src/config/razorpay.ts
@@ -7,5 +7,5 @@ dotenv.config();
 
 export const instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY as string,
-    key_secret: process.env.RAZORPAY_SECRET as string,
+    key_secret: process.env.RAZORPAY_Secret as string
 });
